28f7e65f7bbdbda2fd658a4a633c75c1f5de3975
dimatk01 <dmitriy.tkachuk@4k.com.ua>
2024-03-29 08:12:17 +0200

    add swagger v3 and serve

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,9 @@ import { clientPg } from './connection/postgres-connection'
 import { setupRouter } from './router'
 import Router from 'koa-router'
 import { responseMiddleware } from './middleware/responseMiddleware'
+import {join} from "path"
+import { koaSwagger } from 'koa2-swagger-ui';
+import serve from 'koa-static'
 
 config()
 
@@ -18,13 +21,17 @@ async function startServer() {
   setupRouter(router)
 
   app
+    .use(koaSwagger({routePrefix: "docs", swaggerOptions:{
+        url: "http://localhost:3000/swagger3.json"
+      }}))
+    .use(serve(join(__dirname)))
     .use(responseMiddleware)
     .use(cors())
     .use(bodyParser())
     .use(router.routes())
     .use(router.allowedMethods())
     .use(errorMiddleware)
-
+  console.log(join(__dirname,"..","public"))
   const port = process.env.APP_PORT
   router.stack.forEach((layer) => logger.info('Mapped ' + layer.path))
   app.listen(port, () => logger.info(`Server started at ${port} port`))
